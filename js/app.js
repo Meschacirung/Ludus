13a3ed977ea7d7814375215a0cb31528c334da66
MLHocalhost <18cy039@esisalama.org>
2021-07-09 11:59:39 +0200

    feat (rooms) : create filters drawer

--- js/app.js
+++ js/app.js
@@ -1,6 +1,14 @@
+
+/*
 let menu = document.querySelector('#menu')
 let nav = document.querySelector('#navlinks').classList
+*/
+let filterBtn = document.querySelector('#fBtn')
+let filterD = document.querySelector('#fDismiss')
+let filters = document.querySelector('#filters').classList
 
+console.log(filters)
+/*
 menu.addEventListener('click', function(){
     if (nav.contains('hidden')){
         nav.remove('hidden')
@@ -17,4 +25,14 @@ menu.addEventListener('click', function(){
             nav.add('hidden')
         }.bind(this), 300)
     }
+})
+*/
+filterBtn.addEventListener('click', function(){
+    filters.remove('-mb-96')
+})
+
+filterD.addEventListener('click', function(){
+
+    filters.add('-mb-96')
+    
 })
