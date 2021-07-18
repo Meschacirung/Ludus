0660286369fc16db0492924db7bfa2e6592b103f
MLHocalhost <18cy039@esisalama.org>
2021-07-18 17:43:20 +0200

    feat (chat) : add interactions

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-let menu = document.querySelector('#menu')
+/*let menu = document.querySelector('#menu')
 let nav = document.querySelector('#navlinks').classList
 let filterBtn = document.querySelector('#fBtn')
 let filterD = document.querySelector('#fDismiss')
@@ -28,4 +28,41 @@ filterD.addEventListener('click', function(){
 
     filters.add('-mb-96')
     
+})*/
+let homebtn = document.querySelector('#home')
+let chatbtn = document.querySelector('#chat')
+let chatui = document.querySelector('.chat').classList
+
+let bubble1 = document.querySelector('#bubble1').classList
+let bubble2 = document.querySelector('#bubble2').classList
+
+let eventCal = document.querySelector('#eventCal').classList
+let eventCheck = document.querySelector('#eventCheck').classList
+
+chatbtn.addEventListener('click', function(){
+    chatui.remove('invisible', 'h-0', 'scale-90', 'rounded-2xl')
+    chatui.add('h-screen', 'scale-100')
+
+    chatbtn.classList.replace('text-gray-500', 'text-green-500')
+    bubble1.replace('stroke-current', 'fill-current')
+    bubble1.replace('text-gray-500', 'text-green-500')
+    bubble2.replace('text-gray-300', 'text-green-300')
+
+    eventCal.replace('text-green-300', 'text-gray-500')
+    eventCheck.replace('text-green-500', 'text-gray-300')
+    homebtn.classList.replace('text-green-500', 'text-gray-500')
+})
+
+homebtn.addEventListener('click', function(){
+    chatui.add('invisible', 'h-0', 'scale-90', 'rounded-2xl')
+    chatui.remove('h-screen', 'scale-100')
+
+    chatbtn.classList.replace('text-green-500', 'text-gray-500')
+    bubble1.replace('fill-current', 'stroke-current')
+    bubble1.replace('text-green-500','text-gray-500')
+    bubble2.replace('text-green-300', 'text-gray-300')
+
+    eventCal.replace('text-gray-500', 'text-green-300')
+    eventCheck.replace('text-gray-300', 'text-green-500')
+    homebtn.classList.replace('text-gray-500', 'text-green-500')
 })
